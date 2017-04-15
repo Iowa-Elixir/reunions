e2d11a66f5891067f9ab4beb8f905316e27b01c8
Cliff Braton <cliff.braton@gmail.com>
2017-04-15 13:51:32 -0500

    Retrieve test database credentials from the environment.

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,8 @@ config :logger, level: :warn
 # Configure your database
 config :reunions, Reunions.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DB_USERNAME"),
+  password: System.get_env("DB_PASSWORD"),
   database: "reunions_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
